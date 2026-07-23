123a20918c99c3822b5cb71f5fbbb013971b2880
cuongdz <cuongvip3456789@gmail.com>
2026-07-23 22:23:41 +0700

    nsd

--- api/services/sitemap-generator.js
+++ api/services/sitemap-generator.js
@@ -42,6 +42,53 @@ function todayStr() {
 }
 
 /**
+ * Simple Vietnamese-to-slug conversion
+ * Chuyển "Tiên Hiệp" -> "tien-hiep", "Đại Cổ" -> "dai-co"
+ */
+function slugify(text) {
+  if (!text) return '';
+  var s = String(text).toLowerCase();
+  // Chuyển chữ có dấu sang không dấu
+  var map = {
+    'à':'a','á':'a','ạ':'a','ả':'a','ã':'a',
+    'â':'a','ầ':'a','ấ':'a','ậ':'a','ẩ':'a','ẫ':'a',
+    'ă':'a','ằ':'a','ắ':'a','ặ':'a','ẳ':'a','ẵ':'a',
+    'è':'e','é':'e','ẹ':'e','ẻ':'e','ẽ':'e',
+    'ê':'e','ề':'e','ế':'e','ệ':'e','ể':'e','ễ':'e',
+    'ì':'i','í':'i','ị':'i','ỉ':'i','ĩ':'i',
+    'ò':'o','ó':'o','ọ':'o','ỏ':'o','õ':'o',
+    'ô':'o','ồ':'o','ố':'o','ộ':'o','ổ':'o','ỗ':'o',
+    'ơ':'o','ờ':'o','ớ':'o','ợ':'o','ở':'o','ỡ':'o',
+    'ù':'u','ú':'u','ụ':'u','ủ':'u','ũ':'u',
+    'ư':'u','ừ':'u','ứ':'u','ự':'u','ử':'u','ữ':'u',
+    'ỳ':'y','ý':'y','ỵ':'y','ỷ':'y','ỹ':'y',
+    'đ':'d',
+    'À':'a','Á':'a','Ạ':'a','Ả':'a','Ã':'a',
+    'Â':'a','Ầ':'a','Ấ':'a','Ậ':'a','Ẩ':'a','Ẫ':'a',
+    'Ă':'a','Ằ':'a','Ắ':'a','Ặ':'a','Ẳ':'a','Ẵ':'a',
+    'È':'e','É':'e','Ẹ':'e','Ẻ':'e','Ẽ':'e',
+    'Ê':'e','Ề':'e','Ế':'e','Ệ':'e','Ể':'e','Ễ':'e',
+    'Ì':'i','Í':'i','Ị':'i','Ỉ':'i','Ĩ':'i',
+    'Ò':'o','Ó':'o','Ọ':'o','Ỏ':'o','Õ':'o',
+    'Ô':'o','Ồ':'o','Ố':'o','Ộ':'o','Ổ':'o','Ỗ':'o',
+    'Ơ':'o','Ờ':'o','Ớ':'o','Ợ':'o','Ở':'o','Ỡ':'o',
+    'Ù':'u','Ú':'u','Ụ':'u','Ủ':'u','Ũ':'u',
+    'Ư':'u','Ừ':'u','Ứ':'u','Ự':'u','Ử':'u','Ữ':'u',
+    'Ỳ':'y','Ý':'y','Ỵ':'y','Ỷ':'y','Ỹ':'y',
+    'Đ':'d'
+  };
+  for (var k in map) {
+    var re = new RegExp(k, 'g');
+    s = s.replace(re, map[k]);
+  }
+  // Thay space bằng -, xóa ký tự đặc biệt
+  s = s.replace(/[^a-z0-9-]/g, '-');
+  s = s.replace(/-+/g, '-');
+  s = s.replace(/^-|-$/g, '');
+  return s;
+}
+
+/**
  * Get the latest timestamp from a table column for lastmod accuracy
  */
 async function getLastmodDate(table, dateColumn) {
@@ -278,7 +325,8 @@ async function generateSitemapVideoCategories() {
     for (var i = 0; i < genresResult.rows.length; i++) {
       var name = genresResult.rows[i].name;
       if (!name) continue;
-      var catUrl = BASE_URL + '/video-reviews.html?genre=' + encodeURIComponent(name);
+      var slug = slugify(name);
+      var catUrl = BASE_URL + '/video-reviews.html?genre=' + slug;
       xml += '  <url>\n';
       xml += '    <loc>' + catUrl + '</loc>\n';
       xml += '    <lastmod>' + today + '</lastmod>\n';
@@ -291,7 +339,8 @@ async function generateSitemapVideoCategories() {
     for (var j = 0; j < stylesResult.rows.length; j++) {
       var style = stylesResult.rows[j].name;
       if (!style) continue;
-      var styleUrl = BASE_URL + '/video-reviews.html?style=' + encodeURIComponent(style);
+      var styleSlug = slugify(style);
+      var styleUrl = BASE_URL + '/video-reviews.html?style=' + styleSlug;
       xml += '  <url>\n';
       xml += '    <loc>' + styleUrl + '</loc>\n';
       xml += '    <lastmod>' + today + '</lastmod>\n';
@@ -304,7 +353,8 @@ async function generateSitemapVideoCategories() {
     for (var k = 0; k < subGenresResult.rows.length; k++) {
       var sub = subGenresResult.rows[k].name;
       if (!sub) continue;
-      var subUrl = BASE_URL + '/video-reviews.html?sub_genre=' + encodeURIComponent(sub);
+      var subSlug = slugify(sub);
+      var subUrl = BASE_URL + '/video-reviews.html?sub_genre=' + subSlug;
       xml += '  <url>\n';
       xml += '    <loc>' + subUrl + '</loc>\n';
       xml += '    <lastmod>' + today + '</lastmod>\n';
